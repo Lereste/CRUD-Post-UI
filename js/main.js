089f93428e58ec0d072600274a87ac57c957bac9
Fillbert <Fillbert@DELLXPS13-9370>
2021-11-27 02:35:49 +0700

    Setup axiosClient and API file

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
-console.log('Hello main.js');
+import axiosClient from './api/axiosClient';
+import postApi from './api/postApi';
+
+console.log('Hello from main.js');
+
+async function main() {
+  //   const response = await axiosClient.get('/posts');
+  try {
+    const queryParams = {
+      _page: 1,
+      _limit: 5
+    };
+
+    const response = await postApi.getAll(queryParams);
+    console.log(response);
+  } catch (error) {}
+
+  await postApi.updateFormData({
+    id: 'sktwi1cgkkuif36dj',
+    title: 'Dicta molestiae aut 444'
+  });
+}
+
+main();
